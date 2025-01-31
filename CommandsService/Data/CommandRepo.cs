019e3bde41c9d07591359097f0e3240ef710a7d5
drsplashfire <splashfiredragon@gmail.com>
2025-01-31 16:44:06 +0530

    Added dtos and models, commandrepo

---
+++ CommandsService/Data/CommandRepo.cs
@@ -0,0 +1,81 @@
+﻿using CommandsService.Models;
+//using PlatformService.Models;
+
+namespace CommandsService.Data
+{
+    public class CommandRepo : IcommandRepo
+    {
+        private readonly AppDbContext _context;
+
+        public CommandRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateCommand(Command command, int platformId)
+        {
+            if ( command == null )
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            command.PlatformId = platformId;
+            _context.Commands.Add(command);
+            
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if ( platform == null )
+            {
+                throw new ArgumentNullException(nameof(platform)); 
+            }
+            else 
+            {
+                _context.Add(platform);
+            }
+        }
+
+        public void DeleteCommand(int commandId)
+        {
+            throw new NotImplementedException( );
+        }
+
+        public bool DeletePlatformById(int platformId)
+        {
+            if ( PlatformExists(platformId) )
+            {
+                _context.Remove(platformId);
+                return true;
+            }
+            return false;
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms( )
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Command GetCommand(int platformId, int commandId)
+        {
+            return _context.Commands.Where(c => c.PlatformId == platformId && c.Id == commandId).FirstOrDefault( );
+
+        }
+
+        public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+        {
+            return _context.Commands.Where(c => c.PlatformId == platformId)
+                .OrderBy(c => c.Platform.Name);
+        }
+
+        public bool PlatformExists(int platformId)
+        {
+            return _context.Platforms.Any(c => c.Id == platformId);
+        }
+
+        public bool SaveChanges( )
+        {
+            return _context.SaveChanges( ) >= 0;
+        }
+    }
+}
---
+++ CommandsService/Data/IcommandRepo.cs
@@ -0,0 +1,25 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public interface IcommandRepo
+    {
+        bool SaveChanges( );
+       
+        #region platforms
+        IEnumerable<Platform> GetAllPlatforms();
+        bool DeletePlatformById(int platformId);
+        void CreatePlatform(Platform platform);
+        bool PlatformExists(int platformId);
+        #endregion
+
+        #region Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void DeleteCommand(int commandId);
+        void CreateCommand(Command command, int platformId);
+
+        #endregion
+
+    }
+}
