e80e7bdd2ca1a50d1581ba058ae8bb1eadfef893
drsplashfire <splashfiredragon@gmail.com>
2025-02-03 16:57:19 +0530

    asyncservices

---
+++ platformservice/AsyncDataServices/MessageBusClient.cs
@@ -0,0 +1,24 @@
+﻿using PlatformService.Dtos;
+
+namespace PlatformService.AsyncDataServices
+{
+    public class MessageBusClient : IMessageBusClient
+    {
+        private readonly IConfiguration _configuration;
+
+        public MessageBusClient(IConfiguration configuration)
+        {
+            _configuration= configuration;
+            var factory = new
+            {
+                HostName = _configuration["RabbitMQHost"],
+                Port = int.Parse(_configuration["RabbitMQPort"])
+            };
+        }
+
+        public void PublishNewPlatform(PlatformPublishedDto platformPublishedDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
